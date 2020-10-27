7a1e3c76996f9f8351555a4c5d4a935318008104
Ferdinand Hofherr <ferdinand.hofherr@hetzner-cloud.de>
2020-10-27 13:43:03 +0100

    Add cache for cluster servers
    Internal reference: closes #48

---
+++ internal/hcops/server.go
@@ -0,0 +1,126 @@
+package hcops
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"sync"
+	"time"
+
+	"github.com/hetznercloud/hcloud-go/hcloud"
+)
+
+// AllServersCache caches the result of the LoadFunc and provides random access
+// to servers using select hcloud.Server attributes.
+//
+// To simplify things the allServersCache reloads all servers on every cache
+// miss, or whenever a timeout expired.
+type AllServersCache struct {
+	LoadFunc    func(context.Context) ([]*hcloud.Server, error)
+	LoadTimeout time.Duration
+	MaxAge      time.Duration
+
+	lastRefresh time.Time
+	byPrivIP    map[string]*hcloud.Server
+	byName      map[string]*hcloud.Server
+
+	mu sync.Mutex // protects by* maps
+}
+
+// ByPrivateIP obtains a server from the cache using the IP of one of its
+// private networks.
+//
+// Note that a pointer to the object stored in the cache is returned. Modifying
+// this object affects the cache and all other code parts holding a reference.
+// Furthermore modifying the returned server is not concurrency safe.
+func (c *AllServersCache) ByPrivateIP(ip net.IP) (*hcloud.Server, error) {
+	const op = "hcops/AllServersCache.ByPrivateIP"
+
+	srv, err := c.getCache(func() (*hcloud.Server, bool) {
+		srv, ok := c.byPrivIP[ip.String()]
+		return srv, ok
+	})
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return srv, nil
+}
+
+// ByName obtains a server from the cache using the servers name.
+//
+// Note that a pointer to the object stored in the cache is returned. Modifying
+// this object affects the cache and all other code parts holding a reference.
+// Furthermore modifying the returned server is not concurrency safe.
+func (c *AllServersCache) ByName(name string) (*hcloud.Server, error) {
+	const op = "hcops/AllServersCache.ByName"
+
+	srv, err := c.getCache(func() (*hcloud.Server, bool) {
+		srv, ok := c.byName[name]
+		return srv, ok
+	})
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return srv, nil
+}
+
+func (c *AllServersCache) getCache(getSrv func() (*hcloud.Server, bool)) (*hcloud.Server, error) {
+	const op = "hcops/AllServersCache.getCache"
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	// First try to get the value from the cache if the cache is not yet
+	// expired.
+	if srv, ok := getSrv(); ok && !c.isExpired() {
+		return srv, nil
+	}
+
+	// Reload from the backend API if we didn't find srv.
+	to := c.LoadTimeout
+	if to == 0 {
+		to = 5 * time.Second
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), to)
+	defer cancel()
+
+	srvs, err := c.LoadFunc(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	// Re-initialize all maps. This effectively clears the current cache.
+	c.byPrivIP = make(map[string]*hcloud.Server)
+	c.byName = make(map[string]*hcloud.Server)
+
+	for _, srv := range srvs {
+		// Index servers by the IPs of their private networks
+		for _, n := range srv.PrivateNet {
+			if n.IP == nil {
+				continue
+			}
+			c.byPrivIP[n.IP.String()] = srv
+		}
+
+		// Index servers by their names.
+		c.byName[srv.Name] = srv
+	}
+
+	c.lastRefresh = time.Now()
+
+	// Re-try to find the server after the reload.
+	if srv, ok := getSrv(); ok {
+		return srv, nil
+	}
+	return nil, fmt.Errorf("%s: %w", op, ErrNotFound)
+}
+
+func (c *AllServersCache) isExpired() bool {
+	maxAge := c.MaxAge
+	if maxAge == 0 {
+		maxAge = 10 * time.Minute
+	}
+	return time.Now().After(c.lastRefresh.Add(maxAge))
+}
---
+++ internal/hcops/server_test.go
@@ -0,0 +1,231 @@
+package hcops_test
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"testing"
+	"time"
+
+	"github.com/hetznercloud/hcloud-cloud-controller-manager/internal/hcops"
+	"github.com/hetznercloud/hcloud-cloud-controller-manager/internal/mocks"
+	"github.com/hetznercloud/hcloud-go/hcloud"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestAllServersCache_CacheMiss(t *testing.T) {
+	srv := &hcloud.Server{
+		ID:   12345,
+		Name: "cache-miss",
+		PrivateNet: []hcloud.ServerPrivateNet{
+			{
+				IP: net.ParseIP("10.0.0.2"),
+			},
+		},
+	}
+	cacheOps := newAllServersCacheOps(t, srv)
+	tmpl := allServersCacheTestCase{
+		SetUp: func(t *testing.T, tt *allServersCacheTestCase) {
+			tt.ServerClient.
+				On("All", mock.Anything).
+				Return([]*hcloud.Server{srv}, nil)
+		},
+		Expected: srv,
+	}
+
+	runAllServersCacheTests(t, "Cache miss", tmpl, cacheOps)
+}
+
+func TestAllServersCache_CacheHit(t *testing.T) {
+	srv := &hcloud.Server{
+		ID:   54321,
+		Name: "cache-hit",
+		PrivateNet: []hcloud.ServerPrivateNet{
+			{
+				IP: net.ParseIP("10.0.0.3"),
+			},
+		},
+	}
+	cacheOps := newAllServersCacheOps(t, srv)
+	tmpl := allServersCacheTestCase{
+		SetUp: func(t *testing.T, tt *allServersCacheTestCase) {
+			tt.ServerClient.
+				On("All", mock.Anything).
+				Return([]*hcloud.Server{srv}, nil).
+				Once()
+
+			// Perform any cache op to initialize caches
+			if _, err := tt.Cache.ByName(srv.Name); err != nil {
+				t.Fatalf("SetUp: %v", err)
+			}
+		},
+		Assert: func(t *testing.T, tt *allServersCacheTestCase) {
+			// All must be called only once. This call has happened during the
+			// test SetUp method. All additional calls indicate an error.
+			tt.ServerClient.AssertNumberOfCalls(t, "All", 1)
+		},
+		Expected: srv,
+	}
+
+	runAllServersCacheTests(t, "Cache hit", tmpl, cacheOps)
+}
+
+func TestAllServersCache_CacheRefresh(t *testing.T) {
+	srv := &hcloud.Server{
+		ID:   56789,
+		Name: "cache-refresh",
+		PrivateNet: []hcloud.ServerPrivateNet{
+			{
+				IP: net.ParseIP("10.0.0.9"),
+			},
+		},
+	}
+	cacheOps := newAllServersCacheOps(t, srv)
+	tmpl := allServersCacheTestCase{
+		SetUp: func(t *testing.T, tt *allServersCacheTestCase) {
+			tt.ServerClient.
+				On("All", mock.Anything).
+				Return([]*hcloud.Server{srv}, nil)
+
+			if _, err := tt.Cache.ByName(srv.Name); err != nil {
+				t.Fatalf("SetUp: %v", err)
+			}
+			// Set the maximum cache age to a ridiculously low time to
+			// speed the test up.
+			tt.Cache.MaxAge = time.Nanosecond
+			time.Sleep(2 * tt.Cache.MaxAge)
+		},
+		Assert: func(t *testing.T, tt *allServersCacheTestCase) {
+			// All must be called only twice. Once during set-up and once
+			// during the refresh because of the cache age expired.
+			tt.ServerClient.AssertNumberOfCalls(t, "All", 2)
+		},
+		Expected: srv,
+	}
+
+	runAllServersCacheTests(t, "Cache refresh", tmpl, cacheOps)
+}
+
+func TestAllServersCache_NotFound(t *testing.T) {
+	srv := &hcloud.Server{
+		ID:   101010,
+		Name: "not-found",
+		PrivateNet: []hcloud.ServerPrivateNet{
+			{
+				IP: net.ParseIP("10.0.0.4"),
+			},
+		},
+	}
+	cacheOps := newAllServersCacheOps(t, srv)
+	tmpl := allServersCacheTestCase{
+		SetUp: func(t *testing.T, tt *allServersCacheTestCase) {
+			tt.ServerClient.
+				On("All", mock.Anything).
+				Return(nil, nil)
+		},
+		ExpectedErr: hcops.ErrNotFound,
+	}
+
+	runAllServersCacheTests(t, "Not found", tmpl, cacheOps)
+}
+
+func TestAllServersCache_ClientError(t *testing.T) {
+	err := errors.New("client-error")
+	srv := &hcloud.Server{
+		ID:   202020,
+		Name: "client-error",
+		PrivateNet: []hcloud.ServerPrivateNet{
+			{
+				IP: net.ParseIP("10.0.0.5"),
+			},
+		},
+	}
+	cacheOps := newAllServersCacheOps(t, srv)
+	tmpl := allServersCacheTestCase{
+		SetUp: func(t *testing.T, tt *allServersCacheTestCase) {
+			tt.ServerClient.
+				On("All", mock.Anything).
+				Return(nil, err)
+		},
+		ExpectedErr: err,
+	}
+
+	runAllServersCacheTests(t, "Not found", tmpl, cacheOps)
+}
+
+type allServersCacheOp func(c *hcops.AllServersCache) (*hcloud.Server, error)
+
+func newAllServersCacheOps(t *testing.T, srv *hcloud.Server) map[string]allServersCacheOp {
+	return map[string]allServersCacheOp{
+		"ByPrivateIP": func(c *hcops.AllServersCache) (*hcloud.Server, error) {
+			if len(srv.PrivateNet) == 0 {
+				t.Fatal("ByPrivateIP: server has no private net")
+			}
+			if len(srv.PrivateNet) > 1 {
+				t.Fatal("ByPrivateIP: server more than one private net")
+			}
+			ip := srv.PrivateNet[0].IP
+			if ip == nil {
+				t.Fatal("ByPrivateIP: server has no private ip")
+			}
+			return c.ByPrivateIP(ip)
+		},
+		"ByName": func(c *hcops.AllServersCache) (*hcloud.Server, error) {
+			if srv.Name == "" {
+				t.Fatal("ByName: server has no name")
+			}
+			return c.ByName(srv.Name)
+		},
+	}
+}
+
+type allServersCacheTestCase struct {
+	SetUp       func(t *testing.T, tt *allServersCacheTestCase)
+	CacheOp     allServersCacheOp
+	Expected    *hcloud.Server
+	ExpectedErr error
+	Assert      func(t *testing.T, tt *allServersCacheTestCase)
+
+	// set in run method.
+	ServerClient *mocks.ServerClient
+	Cache        *hcops.AllServersCache
+}
+
+func (tt *allServersCacheTestCase) run(t *testing.T) {
+	tt.ServerClient = mocks.NewServerClient(t)
+	tt.Cache = &hcops.AllServersCache{
+		LoadFunc: tt.ServerClient.All,
+	}
+
+	if tt.SetUp != nil {
+		tt.SetUp(t, tt)
+	}
+	actual, err := tt.CacheOp(tt.Cache)
+	if tt.ExpectedErr == nil && !assert.NoError(t, err) {
+		return
+	}
+	if tt.ExpectedErr != nil {
+		assert.Truef(t, errors.Is(err, tt.ExpectedErr), "expected error: %v; got %v", tt.ExpectedErr, err)
+		return
+	}
+	assert.Equal(t, tt.Expected, actual)
+
+	if tt.Assert != nil {
+		tt.Assert(t, tt)
+	}
+	tt.ServerClient.AssertExpectations(t)
+}
+
+func runAllServersCacheTests(
+	t *testing.T, name string, tmpl allServersCacheTestCase, cacheOps map[string]allServersCacheOp,
+) {
+	for opName, op := range cacheOps {
+		assert.Nil(t, tmpl.CacheOp) // just make sure tmpl was not modified
+
+		// Copy tmpl as we do not want to modify the original
+		tt := tmpl
+		tt.CacheOp = op
+		t.Run(fmt.Sprintf("%s: %s", name, opName), tt.run)
+	}
+}
