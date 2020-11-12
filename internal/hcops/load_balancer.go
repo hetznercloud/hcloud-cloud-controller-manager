3bd93a387572d8566e5ccf404c40ec1045b914a8
Cedric Lewe <43417046+QuByte-Zer0@users.noreply.github.com>
2020-11-12 17:59:37 +0100

    Remove debug logs

--- internal/hcops/load_balancer.go
+++ internal/hcops/load_balancer.go
@@ -346,15 +346,10 @@ func (l *LoadBalancerOps) ReconcileHCLBTargets(
 	usePrivateIP, err := annotation.LBUsePrivateIP.BoolFromService(svc)
 	if err != nil {
 		if errors.Is(err, annotation.ErrNotSet) {
-			klog.Info("Not set")
 			if usePrivateIPDefault, ok := os.LookupEnv(hcloudLoadBalancersDefaultUsePrivateIPENVVar); ok {
 				if usePrivateIP, err = strconv.ParseBool(usePrivateIPDefault); err != nil {
-					klog.Info("to bool failed")
 					return changed, fmt.Errorf("%s: %w", op, err)
 				}
-				klog.Info("ok")
-			} else {
-				klog.Info("no default")
 			}
 		} else {
 			return changed, fmt.Errorf("%s: %w", op, err)
