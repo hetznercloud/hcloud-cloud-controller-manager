fd0b8cec42b87be0681d864e7f8d861577339881
Tibor Djurica Potpara <tibor.djurica@ojdip.net>
2021-06-07 12:22:28 +0100

    Use ::1 host of the IPv6 subnet as the instance address

--- hcloud/instances.go
+++ hcloud/instances.go
@@ -161,9 +161,13 @@ func (i *instances) nodeAddresses(ctx context.Context, server *hcloud.Server) []
 	)
 
 	if i.addressFamily == AddressFamilyIPv6 || i.addressFamily == AddressFamilyDualStack {
+		// For a given IPv6 network of 2001:db8:1234::/64, the instance address is 2001:db8:1234::1
+		host_address := server.PublicNet.IPv6.IP
+		host_address[len(host_address)-1] |= 0x01
+
 		addresses = append(
 			addresses,
-			v1.NodeAddress{Type: v1.NodeExternalIP, Address: server.PublicNet.IPv6.IP.String()},
+			v1.NodeAddress{Type: v1.NodeExternalIP, Address: host_address.String()},
 		)
 	}
 
