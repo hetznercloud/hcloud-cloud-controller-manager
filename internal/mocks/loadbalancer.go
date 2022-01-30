2873cb36bce877b8bc0ee3e84e46cf10c1d5f9ac
Morre <morre@mor.re>
2022-01-30 15:12:45 +0100

    feat: allow setting of reverse DNS records

--- internal/mocks/loadbalancer.go
+++ internal/mocks/loadbalancer.go
@@ -63,6 +63,13 @@ func (m *LoadBalancerClient) ChangeAlgorithm(
 	return getActionPtr(args, 0), getResponsePtr(args, 1), args.Error(2)
 }
 
+func (m *LoadBalancerClient) ChangeDNSPtr(
+	ctx context.Context, lb *hcloud.LoadBalancer, ip string, ptr *string,
+) (*hcloud.Action, *hcloud.Response, error) {
+	args := m.Called(ctx, lb, ip, ptr)
+	return getActionPtr(args, 0), getResponsePtr(args, 1), args.Error(2)
+}
+
 func (m *LoadBalancerClient) ChangeType(
 	ctx context.Context, lb *hcloud.LoadBalancer, opts hcloud.LoadBalancerChangeTypeOpts,
 ) (*hcloud.Action, *hcloud.Response, error) {
