ece517cc8f5503298e19167c7e13bb2942c0b7b8
Sam Day <me@samcday.com>
2023-03-15 16:14:50 +0100

    chore(chart): maintain backwards compat for static manifests

--- chart/templates/_helpers.tpl
+++ chart/templates/_helpers.tpl
@@ -3,6 +3,5 @@
 {{- end }}
 
 {{- define "hcloud-cloud-controller-manager.selectorLabels" -}}
-app.kubernetes.io/name: {{ include "hcloud-cloud-controller-manager.name" $ }}
-app.kubernetes.io/instance: {{ $.Release.Name }}
+{{- tpl (toYaml $.Values.selectorLabels) $ }}
 {{- end }}
--- scripts/generate-deployment-yamls.sh
+++ scripts/generate-deployment-yamls.sh
@@ -13,8 +13,9 @@ fi
 sed -e "s/version: .*/version: $VERSION/" --in-place chart/Chart.yaml
 
 # Template the chart with pre-built values to get the legacy deployment files
-helm template chart > deploy/ccm.yaml
-helm template chart --set networking.enabled=true > deploy/ccm-networks.yaml
+helm_template='helm template chart --set selectorLabels."app\.kubernetes\.io/name"=null,selectorLabels."app\.kubernetes\.io/instance"=null,selectorLabels.app=hcloud-cloud-controller-manager'
+eval $helm_template > deploy/ccm.yaml
+eval $helm_template --set networking.enabled=true > deploy/ccm-networks.yaml
 
 # Package the chart for publishing
 helm package chart
