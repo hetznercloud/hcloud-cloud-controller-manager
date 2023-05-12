e53c0183e9b5e0171cde5f55834a2e0a640d3e5b
samcday <me@samcday.com>
2023-05-12 14:46:42 +0200

    chore(ci): use TPS exclusively (#447)

--- scripts/delete-token.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-token=$1
-curl -A 'travis-terraform-provider' --header 'Authorization: Bearer '"$TTS_TOKEN"'' -X DELETE https://tt-service.hetzner.cloud/token?token=''"$token"''
