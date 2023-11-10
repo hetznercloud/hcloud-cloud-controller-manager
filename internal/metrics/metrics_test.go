173c0729f6cfd4ea43035705af3479dfacc2163d
Maksim Paskal <paskal.maksim@gmail.com>
2023-11-10 13:28:14 +0000

    chore(metrics): add timeouts for metrics server
    Signed-off-by: Maksim Paskal <paskal.maksim@gmail.com>

---
+++ internal/metrics/metrics_test.go
@@ -0,0 +1,42 @@
+package metrics_test
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/hetznercloud/hcloud-cloud-controller-manager/internal/metrics"
+)
+
+func TestMetrics(t *testing.T) {
+	t.Parallel()
+
+	client := &http.Client{}
+	ts := httptest.NewServer(metrics.GetHandler())
+	ctx := context.Background()
+
+	metrics.OperationCalled.WithLabelValues("test").Inc()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, ts.URL, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if m := `cloud_controller_manager_operations_total{op="test"} 1`; !strings.Contains(string(body), m) {
+		t.Fatalf("no metric %s found", m)
+	}
+}
