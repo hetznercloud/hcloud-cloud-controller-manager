7a250e277c4a7c07fb7ba7426633d5a092374442
jo <ljonas@riseup.net>
2024-04-30 16:56:46 +0200

    test: update mocks and tests

--- internal/mocks/action.go
+++ internal/mocks/action.go
@@ -12,18 +12,7 @@ type ActionClient struct {
 	mock.Mock
 }
 
-func (m *ActionClient) WatchProgress(ctx context.Context, a *hcloud.Action) (<-chan int, <-chan error) {
-	args := m.Called(ctx, a)
-	return getIntChan(args, 0), getErrChan(args, 1)
-}
-
-func (m *ActionClient) MockWatchProgress(ctx context.Context, a *hcloud.Action, err error) {
-	resC := make(chan int)
-	errC := make(chan error, 1)
-	if err != nil {
-		errC <- err
-	}
-	close(resC)
-	close(errC)
-	m.On("WatchProgress", ctx, a).Return(resC, errC)
+func (m *ActionClient) WaitFor(ctx context.Context, actions ...*hcloud.Action) error {
+	args := m.Called(ctx, actions)
+	return args.Error(0)
 }
