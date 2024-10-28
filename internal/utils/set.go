018eb616fb90cf359ea0eddc5d3bfdac113c4594
jo <ljonas@riseup.net>
2024-10-28 10:12:10 +0100

    rename to SyncSet
    Co-authored-by: lukasmetzner <lukas.metzner@hetzner-cloud.de>

--- internal/utils/set.go
+++ internal/utils/set.go
@@ -4,34 +4,33 @@ import (
 	"sync"
 )
 
-type MutexSet[T comparable] struct {
+type SyncSet[T comparable] struct {
 	lock sync.Mutex
-
 	data map[T]struct{}
 }
 
-func NewMutexSet[T comparable]() *MutexSet[T] {
-	return &MutexSet[T]{
+func NewSyncSet[T comparable]() *SyncSet[T] {
+	return &SyncSet[T]{
 		lock: sync.Mutex{},
 		data: make(map[T]struct{}, 0),
 	}
 }
 
-func (s *MutexSet[T]) Set(v T) {
+func (s *SyncSet[T]) Set(v T) {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
 	s.data[v] = struct{}{}
 }
 
-func (s *MutexSet[T]) Size() int {
+func (s *SyncSet[T]) Size() int {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
 	return len(s.data)
 }
 
-func (s *MutexSet[T]) All() []T {
+func (s *SyncSet[T]) All() []T {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
--- internal/utils/set_test.go
+++ internal/utils/set_test.go
@@ -7,16 +7,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMutexSet(t *testing.T) {
-	o := NewMutexSet[int]()
+func TestSyncSet(t *testing.T) {
+	o := NewSyncSet[int]()
 
 	o.Set(1)
 	o.Set(2)
 	o.Set(3)
 
 	all := o.All()
-
 	sort.Ints(all)
-
 	assert.Equal(t, []int{1, 2, 3}, all)
 }
