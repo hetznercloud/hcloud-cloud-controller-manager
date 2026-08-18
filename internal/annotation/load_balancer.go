d30be30ce4abf43ffa153204d9221ab76acac357
lukasmetzner <lukas.metzner@hetzner-cloud.de>
2026-08-18 14:47:46 +0200

    refactor: doc generation type

--- internal/annotation/load_balancer.go
+++ internal/annotation/load_balancer.go
@@ -4,102 +4,83 @@ const (
 	// LBPublicIPv4 is the public IPv4 address assigned to the Load Balancer by
 	// the backend.
 	//
-	// Type: string
 	// Read-only: true
 	LBPublicIPv4 String = "load-balancer.hetzner.cloud/ipv4"
 
 	// LBPublicIPv4RDNS is the reverse DNS record assigned to the IPv4 address of
 	// the Load Balancer.
 	//
-	// Type: string
 	// Read-only: true
 	LBPublicIPv4RDNS String = "load-balancer.hetzner.cloud/ipv4-rdns"
 
 	// LBPublicIPv6 is the public IPv6 address assigned to the Load Balancer by
 	// the backend.
 	//
-	// Type: string
 	// Read-only: true
 	LBPublicIPv6 String = "load-balancer.hetzner.cloud/ipv6"
 
 	// LBPublicIPv6RDNS is the reverse DNS record assigned to the IPv6 address of
 	// the Load Balancer.
 	//
-	// Type: string
 	// Read-only: true
 	LBPublicIPv6RDNS String = "load-balancer.hetzner.cloud/ipv6-rdns"
 
 	// LBIPv6Disabled disables the use of IPv6 for the Load Balancer.
 	// Set this annotation if you use external-dns.
 	//
-	// Type: bool
 	// Default: false
 	LBIPv6Disabled Bool = "load-balancer.hetzner.cloud/ipv6-disabled"
 
 	// LBName is the name of the Load Balancer. The name will be visible in
 	// the Hetzner Cloud API console.
-	//
-	// Type: string
 	LBName String = "load-balancer.hetzner.cloud/name"
 
 	// LBDisablePublicNetwork disables the public network of the Hetzner Cloud
 	// Load Balancer. It will still have a public network assigned, but all
 	// traffic is routed over the private network.
 	//
-	// Type: bool
 	// Default: false
 	LBDisablePublicNetwork Bool = "load-balancer.hetzner.cloud/disable-public-network"
 
 	// LBDisablePrivateIngress disables the use of the private network for
 	// ingress.
 	//
-	// Type: bool
 	// Default: false
 	LBDisablePrivateIngress Bool = "load-balancer.hetzner.cloud/disable-private-ingress"
 
 	// LBUsePrivateIP configures the Load Balancer to use the private IP for
 	// Load Balancer server targets.
 	//
-	// Type: bool
 	// Default: false
 	LBUsePrivateIP Bool = "load-balancer.hetzner.cloud/use-private-ip"
 
 	// LBPrivateIPv4 specifies the IPv4 address to assign to the load balancer in the
 	// private network that it's attached to.
-	//
-	// Type: string
 	LBPrivateIPv4 IP = "load-balancer.hetzner.cloud/private-ipv4"
 
 	// PrivateSubnetIPRange specifies an existing subnet to which the load balancer will be attached.
 	// The value must be in the CIDR notation. The subnet must belong to the network defined
 	// in the CCM configuration and must already exist.
 	// See: https://docs.hetzner.cloud/reference/cloud#network-actions-add-a-subnet-to-a-network
-	//
-	// Type: string
 	PrivateSubnetIPRange String = "load-balancer.hetzner.cloud/private-subnet-ip-range"
 
 	// LBHostname specifies the hostname of the Load Balancer. This will be
 	// used as ingress address instead of the Load Balancer IP addresses if
 	// specified.
-	//
-	// Type: string
 	LBHostname String = "load-balancer.hetzner.cloud/hostname"
 
 	// LBSvcProtocol specifies the protocol of the service.
 	//
-	// Type: tcp | http | https
 	// Default: tcp
 	LBSvcProtocol Protocol = "load-balancer.hetzner.cloud/protocol"
 
 	// LBAlgorithmType specifies the algorithm type of the Load Balancer.
 	//
-	// Type: round_robin | least_connections
 	// Default: round_robin
 	LBAlgorithmType AlgorithmType = "load-balancer.hetzner.cloud/algorithm-type"
 
 	// LBType specifies the type of the Load Balancer.
 	//
-	// Type: string
 	// Default: lb11
 	LBType String = "load-balancer.hetzner.cloud/type"
 
@@ -112,8 +93,6 @@ const (
 	// will lead to the load balancer getting new public IPs assigned.
 	//
 	// Mutually exclusive with [LBNetworkZone].
-	//
-	// Type: string
 	LBLocation String = "load-balancer.hetzner.cloud/location"
 
 	// LBNetworkZone specifies the network zone where the Load Balancer will be
@@ -126,8 +105,6 @@ const (
 	// assigned.
 	//
 	// Mutually exclusive with [LBLocation].
-	//
-	// Type: string
 	LBNetworkZone String = "load-balancer.hetzner.cloud/network-zone"
 
 	// LBNodeSelector can be set to restrict which Nodes are added as targets to the
@@ -138,32 +115,23 @@ const (
 	// updated and an Event is created with the error message.
 	//
 	// Format: https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/#label-selectors
-	//
-	// Type: string
 	LBNodeSelector String = "load-balancer.hetzner.cloud/node-selector"
 
 	// LBSvcProxyProtocol specifies if the Load Balancer services should
 	// use the proxy protocol.
 	//
-	// Type: bool
 	// Default: false
 	LBSvcProxyProtocol Bool = "load-balancer.hetzner.cloud/uses-proxyprotocol"
 
 	// LBSvcHTTPCookieName specifies the cookie name when using  HTTP or HTTPS
 	// as protocol.
-	//
-	// Type: string
 	LBSvcHTTPCookieName String = "load-balancer.hetzner.cloud/http-cookie-name"
 
 	// LBSvcHTTPCookieLifetime specifies the lifetime of the HTTP cookie.
-	//
-	// Type: duration
 	LBSvcHTTPCookieLifetime Duration = "load-balancer.hetzner.cloud/http-cookie-lifetime"
 
 	// LBSvcHTTPTimeoutIdle specifies the idle timeout for the client and
 	// server side. Must be between 30s and 300s.
-	//
-	// Type: duration
 	LBSvcHTTPTimeoutIdle Duration = "load-balancer.hetzner.cloud/http-timeout-idle"
 
 	// LBSvcHTTPCertificateType defines the type of certificate the Load
@@ -175,15 +143,11 @@ const (
 
 	// LBSvcHTTPCertificates a comma separated list of IDs or Names of
 	// Certificates assigned to the service.
-	//
-	// Type: string
 	LBSvcHTTPCertificates Certificates = "load-balancer.hetzner.cloud/http-certificates"
 
 	// LBSvcHTTPManagedCertificateName contains the name of the managed
 	// certificate to create by the Cloud Controller manager. Ignored if
 	// [LBSvcHTTPCertificateType] is missing or set to "uploaded".
-	//
-	// Type: string
 	LBSvcHTTPManagedCertificateName String = "load-balancer.hetzner.cloud/http-managed-certificate-name"
 
 	// LBSvcHTTPManagedCertificateUseACMEStaging tells the cloud controller manager to create
@@ -193,7 +157,6 @@ const (
 	// Users should not use this annotation. There is no guarantee that it
 	// remains or continues to function as it currently functions.
 	//
-	// Type: bool
 	// Default: false
 	// Internal: true
 	LBSvcHTTPManagedCertificateUseACMEStaging Bool = "load-balancer.hetzner.cloud/http-managed-certificate-acme-staging"
@@ -202,76 +165,55 @@ const (
 	// domain names of the managed certificate.
 	//
 	// All domains are used to create a single managed certificate.
-	//
-	// Type: string
 	LBSvcHTTPManagedCertificateDomains Strings = "load-balancer.hetzner.cloud/http-managed-certificate-domains"
 
 	// LBSvcRedirectHTTP create a redirect from HTTP to HTTPS.
 	//
-	// Type: bool
 	// Default: false
 	LBSvcRedirectHTTP Bool = "load-balancer.hetzner.cloud/http-redirect-http"
 
 	// LBSvcHTTPStickySessions enables the sticky sessions feature of Hetzner
 	// Cloud HTTP Load Balancers.
 	//
-	// Type: bool
 	// Default: false
 	LBSvcHTTPStickySessions Bool = "load-balancer.hetzner.cloud/http-sticky-sessions"
 
 	// LBSvcHealthCheckProtocol sets the protocol the health check should be
 	// performed over.
 	//
-	// Type: tcp | http | https
 	// Default: tcp
 	LBSvcHealthCheckProtocol Protocol = "load-balancer.hetzner.cloud/health-check-protocol"
 
 	// LBSvcHealthCheckPort specifies the port the health check is be performed
 	// on.
-	//
-	// Type: int
 	LBSvcHealthCheckPort Int = "load-balancer.hetzner.cloud/health-check-port"
 
 	// LBSvcHealthCheckInterval specifies the interval in which we perform a
 	// health check.
-	//
-	// Type: duration
 	LBSvcHealthCheckInterval Duration = "load-balancer.hetzner.cloud/health-check-interval"
 
 	// LBSvcHealthCheckTimeout specifies the timeout of a single health check.
-	//
-	// Type: duration
 	LBSvcHealthCheckTimeout Duration = "load-balancer.hetzner.cloud/health-check-timeout"
 
 	// LBSvcHealthCheckRetries specifies the number of time a health check is
 	// retried until a target is marked as unhealthy.
-	//
-	// Type: int
 	LBSvcHealthCheckRetries Int = "load-balancer.hetzner.cloud/health-check-retries"
 
 	// LBSvcHealthCheckHTTPDomain specifies the domain we try to access when
 	// performing the health check.
-	//
-	// Type: string
 	LBSvcHealthCheckHTTPDomain String = "load-balancer.hetzner.cloud/health-check-http-domain"
 
 	// LBSvcHealthCheckHTTPPath specifies the path we try to access when
 	// performing the health check.
-	//
-	// Type: string
 	LBSvcHealthCheckHTTPPath String = "load-balancer.hetzner.cloud/health-check-http-path"
 
 	// LBSvcHealthCheckHTTPValidateCertificate specifies whether the health
 	// check should validate the SSL certificate that comes from the target
 	// nodes.
-	//
-	// Type: bool
 	LBSvcHealthCheckHTTPValidateCertificate Bool = "load-balancer.hetzner.cloud/health-check-http-validate-certificate"
 
 	// LBSvcHealthCheckHTTPStatusCodes is a comma separated list of HTTP status
 	// codes which we expect.
-	//
-	// Type: string
 	LBSvcHealthCheckHTTPStatusCodes Strings = "load-balancer.hetzner.cloud/http-status-codes"
 
 	// LBID is the ID assigned to the Hetzner Cloud Load Balancer by the
@@ -279,7 +221,6 @@ const (
 	//
 	// Deprecated: This annotation is not used. It is reserved for possible future use.
 	//
-	// Type: string
 	// Read-only: true
 	LBID String = "load-balancer.hetzner.cloud/id"
 )
